39f55a83c39d01f0e2c3438d2fdf513f51e4d9ba
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2024-11-17 00:25:13 -0600

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use("/api/users", UsuarioRouter)
 app.use("/api/chats", ChatRouter)
 app.use("/api/posts", PostRouter)
