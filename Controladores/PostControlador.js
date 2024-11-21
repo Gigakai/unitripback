da5bd230938ff8d155ade04697ed4a2e17effe4b
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2024-11-20 23:24:35 -0600

    make it better

--- Controladores/PostControlador.js
+++ Controladores/PostControlador.js
@@ -105,7 +105,6 @@ export const deletePost = async (req, res) => {
             return res.status(400).json({ success: false, msg: "Error en los parametros", errors: [] })
         }
 
-        await ImagenesModel.eliminarAntiguos(idPost)
         await PostModel.eliminar(idPost)
 
 
